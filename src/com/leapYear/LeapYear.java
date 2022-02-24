e29c369aff2f33ab43960a78e89e007c35697c0c
smbiradar <smbiradar92@gmail.com>
2022-02-25 00:28:43 +0530

    Leap Year Problem solved

---
+++ src/com/leapYear/LeapYear.java
@@ -0,0 +1,24 @@
+package com.leapYear;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Please Enter the year to check for Leap Year in format YYYY:");
+		
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		
+		System.out.println(year);
+	
+		if ( year % 4 == 0 && year % 400 !=0 ) {
+			System.out.println("The entered year" +" "+ year + " "+ "is a leap year");
+		} else {
+			System.out.println("The entered year" +" "+ year + " "+ "is a not leap year");
+		}
+		
+		sc.close();
+	}
+}
